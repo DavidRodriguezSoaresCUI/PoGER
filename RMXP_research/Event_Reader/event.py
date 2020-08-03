53bc7294fe43730caf5c205d7d4236ded870f2be
David.Rodriguez.1 <david.rodriguez.1@etu.unige.ch>
2020-08-03 02:11:44 +0200

    More doc

--- RMXP_research/Event_Reader/event.py
+++ RMXP_research/Event_Reader/event.py
@@ -1,11 +1,18 @@
+debug = True
+
 class Event:
 
     def __init__(self):
         self.xy=None
         self.config=dict()
         self.behaviors=list()
+        self.current_behavior=None
+        self.triggered = False
+        self.final = False
 
     def add_parameter( self, p ):
+        assert not self.final, 'E.add_parameter: Cannot add parameter to finalized Event.'
+
         if p==None:
             return
         assert isinstance(p, tuple)
@@ -14,6 +21,10 @@ class Event:
         print('add_parameter: p_value {},{}'.format(type(p_value),p_value))
         assert isinstance( p_name, str )
         assert type(p_value) in [int, float, str, list], 'add_parameter: type error : {},{}'.format(type(p_value), p_value)
+
+        if debug:
+            print(f'E:add_parameter : adding parameter {p_name}')
+
         if p_name=='xy':
             # thorough initial position test
             assert isinstance(p_value, list) and len(p_value)==2 and all([isinstance(p_value[i], int) \
@@ -23,31 +34,64 @@ class Event:
             self.config[p_name]=p_value
 
     def add_behavior( self ):
+        assert not self.final, 'E.add_behavior: Cannot add behavior to finalized Event.'
+        if debug:
+            print(f'E:add_behavior : adding a behavior')
+            
         self.behaviors.append(Behavior(self.config))
 
     def behavior_add_parameter( self, p ):
         self.behaviors[-1].add_parameter(p)
 
-    def behavior_add_instruction( self, i ):
-        self.behaviors[-1].add_instruction(i)
+    def behavior_add_instruction( self, **kwargs ):
+        return self.behaviors[-1].add_instruction(**kwargs)
+
+    def choose_behavior(self):
+        valid_b = [ idx for idx,b in enumerate(self.behaviors) if b.satisfies_conditions() ]
+        assert 0 < len(valid_b), 'E.choose_behavior: no behavior satisfies its conditions'
+        self.current_behavior = max(valid_b)
+
+    def finalize_behaviors(self):
+        if not self.final:
+            for b in self.behaviors:
+                b.finalize()
+            self.final = True
 
     def trigger( self, t ):
+        assert self.triggered==False, 'E.trigger : Already triggered.'
+        tri = self.config.get( 'trigger', None )
+        if tri and (tri==t):
+            self.triggered = True
+            self.finalize_behaviors()
+            self.choose_behavior()
+
+    def walk(self):
         pass
 
     def step(self):
-        pass
+        ''' Perform a step : either walking animation or behavior '''
+        if self.triggered:
+            self.triggered = self.behaviors[self.current_behavior].step()
+        if not self.triggered:
+            self.walk()
 
     def __str__(self):
-        return 'Event: '+str(self.__dict__)
+        return 'Event: '+str(self.__dict__) + '\n'.join([ str(b) for b in self.behaviors ])
 
 
 class Behavior:
 
     def __init__(self, cfg):
         self.config=dict(cfg)
+        self.conditions=list()
         self.instructions=list()
+        self.final = False
+        self.tick = 0
 
     def add_parameter(self, p):
+        assert not self.final, 'B.add_parameter: Cannot add parameter to finalized Behavior.'
+        # TODO : separate config and conditions
+
         if p==None:
             return
         assert isinstance(p, tuple)
@@ -56,11 +100,51 @@ class Behavior:
         print('B.add_parameter: p_value {},{}'.format(type(p_value),p_value))
         assert isinstance( p_name, str )
         assert type(p_value) in [int, float, str, list], 'B.add_parameter: type error : {},{}'.format(type(p_value), p_value)
+
+        if debug:
+            print(f'B:add_parameter : adding parameter {p_name}')
+
         if p_name=='xy':
             raise ValueError('unexpected redefinition of parameter "xy"')
         else:
             self.config[p_name]=p_value
 
-    def add_instruction(self):
+    def add_instruction(self, **kwargs):
+        assert not self.final, 'B.add_instruction: Cannot add instruction to finalized Behavior.'
+
+        if debug:
+            print(f'B:add_instruction : adding instruction')
+            for k,v in kwargs.items():
+                print(f'B:add_instruction : k={k},v={v}')
+
+        clean = {k:v for k,v in kwargs.items() if v!=None}
+        self.instructions.append( clean )
+        #self.instructions.append( self.prepare_instruction( i ) )
+        return clean
+
+    def prepare_instruction(self,i):
+        # prepare isntructions, so they can be passed to exec()
         pass
+
+    def finalize( self ):
+        self.final = True
     pass
+
+    def satisfies_conditions(self):
+        return all([ exec(cond) for cond in self.conditions ])
+
+    def step(self):
+        # implement 
+        assert self.final, 'B.step : behavior unfinalized'
+        try:
+            instr = self.instructions[self.tick]
+            # TODO : exec()
+        except IndexError:
+            self.tick = 0
+            return False
+
+        self.tick += 1
+        return True
+
+    def __str__(self):
+        return str(self.__dict__)
--- RMXP_research/Event_Reader/eventReader.py
+++ RMXP_research/Event_Reader/eventReader.py
@@ -1,11 +1,14 @@
 import sys, token, operator 
 from tokenize import generate_tokens
 from io import StringIO
-import pathlib
+import pathlib, logging
 from pprint import pprint
 from funcparserlib.parser import (some, a, many, skip, finished, maybe, with_forward_decls, oneplus)
 from funcparserlib.lexer import make_tokenizer, Token, LexerError
 from event import Event
+
+import funcparserlib.parser
+funcparserlib.parser.debug=True
 
 def tokenize(str):
     '''str -> Sequence(Token)'''
@@ -14,18 +17,20 @@ def tokenize(str):
         ('LF', (r'(\r\n)|\n',)),
         ('Space', (r'[ \t]+',)),
         ('Comma', (r'\,',)),
+        ('Keyword', (r'(if)|(else)|(when)|(loop)|(break)',)),
+        #('Command', (r'',)),
         ('Marker', (r'(\[event\])|(\[page\])|(\[end\])',)),
-        ('Indent', (r'\[INDENT\]',)),
-        ('Dedent', (r'\[DEDENT\]',)),
+        ('Indent', (r'(\[INDENT\])|(\[DEDENT\])',)),
         ('Script', (r's\:[a-z][a-z\_0-9\.]*',)),
         ('Symbol', (r'\:[a-z][a-z\_0-9]*',)),
+        ('Bool', (r'(True)|(False)',)), # basically, a string without quotemarks, delimited by spaces
         ('Word', (r'[a-z][a-z\_0-9]*',)), # basically, a string without quotemarks, delimited by spaces
         ('Log_Op', (r'(==)|(>=)|(<=)|(>)|(<)|(!=)',)),
         ('Assign_Op', (r'[\+\-\*/]?=',)),
         ('Op', (r'[\+\-\*/]',)),
         ('Number', (r'-?[0-9]+(\.[0-9]*)?',)),
         ('Bracket', (r'[\[\]]',)),
-        ('Parenthesis', (r'[\(\)]',)),
+        #('Parenthesis', (r'[\(\)]',)),
         ('String', (r'"[^"]*"',)), # '\"' escapes are ignored
     ]
     useless = ['Comment', 'Space']#['Comment', 'NL', 'Space']
@@ -59,15 +64,17 @@ def parse( tokens ):
     'Sequence(Tokens) -> int/float/None'
 
     e=Event()
+    e.add_behavior()
 
     def make_number(s):
         try:
             return int(s)
         except ValueError:
             return float(s)
-    def eval_expr(z, list):
-        'float, [((float, float -> float), float)] -> float'
-        return reduce(lambda s, f_x: f_x[0](s, f_x[1]), list, z)
+
+    def new_page( x ):
+        e.add_behavior()
+        return x
     
     # const(x) [function] takes no arg and returns x
     const  = lambda x: lambda _: x
@@ -76,8 +83,6 @@ def parse( tokens ):
     tokval = lambda tok: tok.value
     # makeop(s,f) [function] returns an operator : symbol s, behaves like actual operator f
     makeop = lambda s, f: op(s) >> const(f)
-    # eval [function] takes argument(s) and returns 'eval_expr(*args)'
-    eval = unarg(eval_expr)
     assign_op = lambda s: a(Token('Assign_Op', s)) >> tokval
     assign_op_ = lambda s: skip(assign_op(s))
 
@@ -98,17 +103,35 @@ def parse( tokens ):
         some(lambda tok: tok.type=='Comma')
         >> tokval )
 
-    anything = (
+    '''anything = (
         some(lambda tok: tok.type!='LF' and tok.type=='Marker')
+        >> tokval
+    )'''
+
+    symbol = (
+        some(lambda tok: tok.type=='Symbol')
+        >> tokval
+    )
+
+    script = (
+        some(lambda tok: tok.type=='Script')
         >> tokval
     )
 
     a_tok = lambda t_type, t_val: a(Token(t_type, t_val))
     
-    words = many(word)
+    words = oneplus(word)
+
+    # parsing indentation
+    indent = a_tok('Indent', '[INDENT]')
+    dedent = a_tok('Indent', '[DEDENT]')
+
+    # parsing keywords
+    a_if = a_tok('Word', 'if')
+    a_else = a_tok('Word', 'else')
 
     # parsing ops
-    log_op    = lambda o: a_tok('Log_Op', o)
+    log_op = lambda o: a_tok('Log_Op', o)
     equ = log_op('==')
     geq = log_op('>=')
     leq = log_op('<=')
@@ -148,25 +171,26 @@ def parse( tokens ):
     a_marker = lambda t_val: a_tok('Marker', t_val)
     a_marker_ = lambda t_val: skip(a_marker(t_val))
     event_marker = a_marker_('[event]')
-    page_marker  = a_marker_('[page]')
+    page_marker  = a_marker_('[page]') >> new_page
     end_marker   = a_marker_('[end]')
 
     # Parsing line feeds
     LF = a_tok('LF', '\n')
     LF_ = skip(LF)
     LFs = many(LF)
-    LFs_ = skip(LFs)
-
-    #page = page_marker# + config
+    LFs_ = skip(maybe(LFs))
 
     value = number | string | word
 
     def make_array(n):
-        #print(f'make_array: n:{type(n)},{n}')
+        print(f'make_array: n:{type(n)},{n}')
         return [] if (n is None) else [n[0]] + n[1]
 
-    a_list = opening_bracket + value + many(skip(maybe(comma)) + value) + closing_bracket >> make_array
+    def print_and_return( x ):
+        print(f'print_and_return: x,{type(x)}, {x}')
+        return x
 
+    a_list = opening_bracket + value + many(skip(maybe(comma)) + value) + closing_bracket >> make_array
     parameter_value = value | a_list
     config_var = word
 
@@ -175,31 +199,47 @@ def parse( tokens ):
 
     cmd_id = words
     parameter_name = word
-    parameter = maybe(parameter_name + assi) + value
-    parameters = parameter + many( skip(maybe(comma)) + parameter )
-    
-    cmd = cmd_id + maybe(parameters)
-    
-    statement = cmd
-    
-    statements = many(statement + LFs_)
-    config_or_cond = maybe( config_var + assign_op_('=') + parameter_value ) >> deal_with_config
+    parameter = maybe(parameter_name + assi) + value #>> make_array
+    parameters = parameter + many( skip(maybe(comma)) + parameter ) #>> make_array
 
-    page = page_marker + LFs_ + many(config_or_cond + LF_) + LFs_ + maybe(statements) + end_marker + LFs_#
+    def deal_with_cmd(s): 
+        print(f'deal_with_cmd: s,{type(s)},{s}')
+        e.behavior_add_instruction( cmd_id=''.join(s[0]), parameters=s[1] )
+    
+    cmd = cmd_id + many(parameters) >> deal_with_cmd
+
+    comparable = number | symbol | script
+    log_expr = comparable + log_operator + comparable
+
+    @with_forward_decls
+    def code_block(): # hack to use 'statements' above its declaration
+        return indent + statements + dedent
+    if_cond = skip(a_if) + log_expr + LFs_ + code_block + maybe( skip(a_else) + LFs_ + code_block )
+
+    statement = cmd# | if_cond
+    statements = many(statement + LFs_)
+
+    config_or_cond = (maybe( config_var + assign_op_('=') + parameter_value ) >> deal_with_config) \
+        | (log_expr >> print_and_return)
+
+    page = (
+        page_marker + LFs_ + many(config_or_cond + LF_) + LFs_ + statements + end_marker + LFs_
+        >> print_and_return )
 
     end = skip(finished)
-    event = event_marker + LFs_ + oneplus(config + LF_) + LFs_ + oneplus(page) + end
+    event = event_marker + LFs_ + oneplus(config + LF_) + LFs_ + many(page) + end
     
     res = event.parse(tokens)
     print('res:{}'.format(res))
 
     return e
 
-
+def boolean_converter( s ):
+    return s.replace(':ON','True').replace(':OFF','False')
 
 if __name__ == "__main__":
     cwd = pathlib.Path()
-    events = list(cwd.glob('Map001_*'))
+    events = list(cwd.glob('Map000_*'))
     pprint(events)
 
     for event in events:
@@ -208,8 +248,8 @@ if __name__ == "__main__":
             content = f.read().lower()
         
         assert not ( '\t' in content )
-        content_noindent = indentation_converter(content)
-        tok = tokenize( content_noindent )
+        content_ok = boolean_converter( indentation_converter(content) )
+        tok = tokenize( content_ok )
         
         with open('test', 'w') as f:
             for token in tok:
@@ -218,6 +258,6 @@ if __name__ == "__main__":
         
         out = parse( tok )
         print(type(out))
-        print(str(out))
+        pprint(str(out))
         '''with open('test2', 'w') as f:
             f.write(str(out))'''
